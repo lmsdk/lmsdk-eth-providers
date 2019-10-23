05fc02aa6ac19a7f3bcbd2eff578dae9a985a87e
renkui <371397938@qq.com>
2019-10-23 22:16:04 +0800

    add LMTheme,LMLanguage UNI plugins support code.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const HttpProvider = require('./httpProvider.js');
+const url = require('url');
+const Web3 = require('web3');
+import httpProvider from './package/lmsdk-eth-providers-http';
 
-module.exports.HttpProvider = HttpProvider;
+var localURL = url.parse(location.href, true);
+
+if ( localURL.query._lmt === "ethereum" ) {
+    
+    if (localURL.query._lmv.indexOf("http") === 0) {
+        window.ethereum = new httpProvider(localURL.query._lmv);
+    } else if ( localURL.query._lmv.indexOf("ws") === 0 ) {
+        /// websocket provider
+    }
+    
+    window.web3 = new Web3(window.ethereum);
+}
+
+module.exports = {
+    httpProvider: httpProvider
+};
