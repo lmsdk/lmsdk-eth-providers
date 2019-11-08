8b851d178fd2b8cfc1dc12b0f231ad6f4fa827c7
oblivioned <371397938@qq.com>
2019-11-08 12:52:40 +0800

    add LMCore WebSocketProvider support nodejs code.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let Web3 = require('web3');
 let httpProvider = require('./package/lmsdk-eth-providers-http');
+let wsProvider = require('./package/lmsdk-eth-providers-ws');
 let ethScanApi = require('./package/lmsdk-eth-scan-api');
 
 if ( window.lmdapp && window.lmdapp.lmt === "ethereum" ) {
@@ -7,7 +8,7 @@ if ( window.lmdapp && window.lmdapp.lmt === "ethereum" ) {
     if ( window.lmdapp.lmv.indexOf("http") === 0 ) {
         window.ethereum = new httpProvider(window.lmdapp.lmv);
     } else if ( window.lmdapp.lmv.indexOf("ws") === 0 ) {
-        /// websocket provider
+        window.ethereum = new wsProvider(window.lmdapp.lmv);
     }
     
     window.web3 = new Web3(window.ethereum);
@@ -17,5 +18,6 @@ window.Web3 = Web3;
 
 module.exports = {
     httpProvider: httpProvider,
+    wsProvider: wsProvider,
 	ethScanApi: ethScanApi
 };
