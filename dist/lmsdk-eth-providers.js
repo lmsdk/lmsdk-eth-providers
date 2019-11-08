0e4fecfa5c63bff2d297bdeb7c3c6065115ceb58
oblivioned <371397938@qq.com>
2019-11-08 15:21:36 +0800

    bug

--- dist/lmsdk-eth-providers.js
+++ dist/lmsdk-eth-providers.js
@@ -12,7 +12,7 @@ if ( window.lmdapp && window.lmdapp.lmt === "ethereum" ) {
         window.ethereum = new wsProvider(window.lmdapp.lmv);
     }
     
-    window.ethereum.enable().then();
+    window.ethereum.enable();
     window.web3 = new Web3(window.ethereum);
 }
 
